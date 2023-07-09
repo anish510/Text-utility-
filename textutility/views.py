9d844b92e09dceeeccf5094dad075000e4dd8915
anish510 <anishjoshi510@gmail.com>
2023-07-09 12:40:01 +0545

    addition of features

--- textutility/views.py
+++ textutility/views.py
@@ -8,19 +8,58 @@ def index(request):
  
 def analyze(request):
     djtext = request.GET.get('text','default')
+    
     removepunc = request.GET.get('removepunc','off')
-    print(djtext)
-    print(removepunc)
+    fullcapss = request.GET.get('fullcaps','off')
+    newlineremover = request.GET.get('newlineremover','off')
+    extraspaceremover = request.GET.get('extraspaceremover','off')
+    
     punc = string.punctuation
     analyzed = ''
-    if removepunc == 'on':
+    
+    if removepunc == 'on' and fullcapss == 'off':
         for char in djtext:
             if char not in punc:
                 analyzed = analyzed + char
         params = {'purpose':'Removed Punctuations','analyzed_text': analyzed}   
         return render(request,'analyze.html',params)
+    
+    
+    elif fullcapss == 'on' and removepunc == 'off':
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+        params = {'purpose':'Changed to UpperCase','analyzed_text': analyzed}  
+        return render(request,'analyze.html',params)
+    
+    
+    elif removepunc == 'on' and fullcapss == 'on':
+        for char in djtext:
+            if char not in punc:
+                analyzed = analyzed + char.upper()
+        params = {'purpose':'Removed Punctuations and Changed to UpperCase','analyzed_text': analyzed}
+        return render(request,'analyze.html',params)
+    
+    elif newlineremover == 'on':
+        for char in djtext:
+            if char != '\n':
+                analyzed = analyzed + char
+        params = {'purpose':'New Line Removed','analyzed_text': analyzed}
+        return render(request,'analyze.html',params)
+            
+    elif extraspaceremover == 'on':
+        for index,char in enumerate(djtext):
+            if not(djtext[index] == ' ' and djtext[index+1] == ' '):
+                analyzed = analyzed + char
+                
+            
+        params = {'purpose':'Extra Space Removed','analyzed_text': analyzed}
+        return render(request,'analyze.html',params)
+        
     else:
         return HttpResponse("Error")
+    
+    
+        
 
 def capfirst(request):
     return HttpResponse("capitalize first")
